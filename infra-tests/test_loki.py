ecf65b580f9eb32ad5592f4fa3f4a465701443a1
Oleg Konobeev <okonobeev@dtln.ru>
2022-03-31 21:18:38 +0200

    Litte fix to loki test. Have to use ranges and random log line

--- infra-tests/test_loki.py
+++ infra-tests/test_loki.py
@@ -1,6 +1,8 @@
 import pytest
 import requests
 import json
+import time
+import uuid
 
 loki_host = "mon-server.illcheck.you"
 loki_scheme = "http://"
@@ -14,18 +16,30 @@ def test_aliveness():
     response = requests.get(url=loki_api+path)
     assert response.status_code == 200
 
+
 # Here we will start a little docker container
 # with all labels and generate log-string
+#
+# Will use random string for log line
+loki_log_line = str(uuid.uuid4())
+
 def test_docker_log_line_to_loki(host):
     docker_run = host.run("docker run --rm --name my-service "
                            "--log-driver=loki --log-opt loki-url=\"http://localhost:3100/loki/api/v1/push\" "
-                           "-l com.docker.compose.service=myapp alpine sh -c \"echo sEcReT_LiNe\"")
+                           "-l com.docker.compose.service=myapp alpine sh -c \"echo " +
+                            loki_log_line + "\"")
     assert docker_run.rc == 0
 
 # Checking loki API to find a string
 def test_log_line_in_loki():
-    path = "/loki/api/v1/query"
+    # Searching only in range of 8 seconds
+    epoch = int(time.time())
+    start_epoch = epoch - 4
+    end_epoch = epoch + 4
+    path = "/loki/api/v1/query_range"
     query = { 
+        'start': start_epoch,
+        'end': end_epoch,
         'query': '{compose_service="myapp"}',
     }
     response = requests.get(url=loki_api+path, params=query)
@@ -33,4 +47,4 @@ def test_log_line_in_loki():
     assert response.status_code == 200
     assert response.headers["Content-Type"] == "text/plain; charset=utf-8"
     log_line = j_response['data']['result'][0]['values'][0][1]
-    assert log_line == "sEcReT_LiNe"
+    assert log_line == loki_log_line
