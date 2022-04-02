7e5fe0771ca472283e6fc13a6d11d0afb735dbbd
Oleg Konobeev <okonobeev@dtln.ru>
2022-04-02 19:34:51 +0200

    + prom test

---
+++ infra-tests/test_prom_alerts.py
@@ -0,0 +1,34 @@
+import pytest
+import requests
+import json
+
+prom_host = "mon-server.illcheck.you"
+prom_scheme = "http://"
+prom_port = "9090"
+prom_api = prom_scheme + prom_host + ":" + prom_port
+
+
+# Are we alive?
+def test_aliveness():
+    path = "/api/v1/query"
+    query = {
+        'query': 'up',
+    }
+    response = requests.get(url=prom_api+path, params=query)
+    assert response.status_code == 200
+
+# Check that our rule list is correct
+#
+# TODO: 
+# 1. get container name from list of containers by pytest.docker
+# 2. get rule list from somewhere (maybe put in rules dir?)
+def test_check_rules_syntax(host):
+    docker_run = host.run("docker exec monitoring_prometheus_1 "
+                           "sh -c \"promtool check rules /etc/prometheus/alerts.yml \"")
+    assert docker_run.rc == 0
+
+def test_unittest_rules(host):
+    docker_run = host.run("docker exec monitoring_prometheus_1 "
+                           "sh -c \"promtool test rules /etc/prometheus/unittest.yml \"")
+    assert docker_run.rc == 0
+
